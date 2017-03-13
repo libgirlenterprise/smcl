3e6c47855585304fbde25adffac0e09dcf78813c
danielviolin <danielviolin0723@gmail.com>
2017-03-13 16:12:09 +0800

    fixed the wrong structure if reduce-f, but not finished
    had little beginning of invoke-f

--- src/procedure-pool.lisp
+++ src/procedure-pool.lisp
@@ -20,30 +20,60 @@
   ((procedures :type hash-table
 	       :initform (make-hash-table))))
 
-(defgeneric reduce-f (body-list procedure procedure-pool))
+(defgeneric reduce-f (body procedure procedure-pool))
 
 (defgeneric invoke-f (symbol args procedure-pool)) ; should handle when symbol is not a symbol
 
-(defmethod reduce-f (body-list procedure (procedure-pool procedure-pool))
-  (loop for i from 1 below (length body-list)
-	do (setf (nth i body-list)
-		 (let* ((sub-body (nth i body-list))
-			(sub-body (if (atom sub-body)
-				      (list sub-body)
-				      sub-body))
-			(symbol-primitive-p (primitivep (first sub-body))))
-		   (apply (if symbol-primitive-p
-			      #'apply-primitive-f
-			      #'invoke-f)
-			  (let ((args-for-apply (list procedure-pool)))
-			    (if symbol-primitive-p
-				(push procedure-args args-for-apply))
-			    (append (list (first sub-body)
-					  (append (subseq sub-body 1)
-						  procedure-args))
-				    args-for-apply)))))))
-			    
+(defmethod reduce-f (body procedure (procedure-pool procedure-pool))
+  (let* ((old-body-atom-p (atom body))
+	 (old-body (if old-body-atom-p
+		       body
+		       (copy-list body)))
+	 (body-operator (if old-body-atom-p
+			    old-body ; now the same as body unless recursion
+			    (if (atom (first old-body))
+				(first old-body)
+				'quote))) ; even if the first element of the body is not an atom, it should be perfect reduced
+	 (operator-primitive-p (primitivep body-operator)))
 
-		       
-  
-					 
+    ;; reduce sub-procedure before invoke
+    (unless operator-primitive-p
+      (let ((sub-procedure (gethash body-operator
+				    (slot-vaue procedure-pool 'procedures))))
+	(reduce-f sub-procedure-body sub-procedure-params procedure-pool)))
+
+    ;; reduce arguments of this body
+    (unless (or (atom body)
+		(= (length body) 1))
+      (let ((perfect-form))
+	(do* ((i 1 (incf i)))
+	     ((or (atom body) ; for the case being rewriten
+		  (if (>= (- i 1) (length body)) ; for the same case of the last line
+		      t
+		      (progn
+			(setf (nth (- i 1) body)
+			      perfect-form)
+			nil))
+		  (>= i (length body))))
+	  (setf perfect-form (reduce-f (nth i body)
+				       procedure
+				       procedure-pool)))))
+
+    ;; invoke sub-procedure
+    (apply (if operator-primitive-p
+	       #'apply-primitive-f
+	       #'invoke-f)
+	   (append (list body-operator)
+		   (append (if old-body-atom-p
+			       nil
+			       (subseq body 1))
+			   procedure-args)
+		   (if operator-primitive-p
+		       procedure-args)
+		   (list procedure-pool)))))
+			    
+(defmethod invoke-f (symbol args (procedure-pool procedure-pool))
+  (let ((procedure (gethash symbol
+			    (slot-vaue procedure-pool procedures))))
+    (reduce-f procedure-body procedure procedure-pool)
+    

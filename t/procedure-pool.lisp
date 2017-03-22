4f3bc44f05cb0baecf2e09c1f3ea8bce057824c9
danielviolin <danielviolin0723@gmail.com>
2017-03-22 14:01:30 +0800

    test export and non export of com.libgirl.smcl

--- t/procedure-pool.lisp
+++ t/procedure-pool.lisp
@@ -3,12 +3,39 @@
   (:use :cl :com.libgirl.smcl :prove))
 (in-package :com.libgirl.smcl-test.procedure-pool)
 
+(defparameter *non-export-symbol-list* '("*PARAM-SIZE*"
+					 "*ARG-SIZE*"
+					 "*USER-INPUT-FUNCTION*"
+					 "MAKE-USER-INPUT-FUNCTION"
+					 "PROCEDURE"
+					 "PROCEDURE-POOL"
+					 "REDUCE-F"
+					 "INVOKE-F"
+					 "SET-PROCEDURE"
+					 "EXPORT-TO-FILE"))
+
 (defun run-program-for-make-user-input-function (input-stream)
   (sb-ext:run-program "/usr/local/bin/sbcl"
 		      '("--noinform" "--load" "make-user-input-function.lisp")
 		      :output nil
 		      :input input-stream
 		      :wait nil))
+
+;;; test export or non-export of com.libgirl.smcl
+(plan (+ 1 (length *non-export-symbol-list*)))
+(ok (every #'(lambda (symbol)
+	       (ok (not (find-symbol symbol))))
+	   *non-export-symbol-list*))
+(plan (+ 1 (* 2 (length *non-export-symbol-list*))))
+(ok (every #'(lambda (symbol)
+	       (multiple-value-bind (when-symbol status)
+		   (find-symbol symbol 'com.libgirl.smcl)
+		 (ok when-symbol)
+		 (is status :internal)))
+	   *non-export-symbol-list*))
+(plan 1)
+(ok (find-symbol "SMCL-RUN"))
+
 
 (plan 1)
 

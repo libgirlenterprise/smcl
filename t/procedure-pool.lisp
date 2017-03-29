5ce721c821e2058f71a1ed16d21746a0b2b0458f
danielviolin <danielviolin0723@gmail.com>
2017-03-29 17:02:50 +0800

    used keyword for symbols in our own lisp dialect

--- t/procedure-pool.lisp
+++ t/procedure-pool.lisp
@@ -9,12 +9,12 @@
 
 (defparameter *simple-case-data-size* 3)
 
-(defparameter *simple-case-data* (list (list 'x 'y 'z)
+(defparameter *simple-case-data* (list (list :x :y :z)
 				       (make-list *simple-case-data-size*)
 				       (make-list *simple-case-data-size*
 						  :initial-element (make-list com.libgirl.smcl::*arg-size*
 									      :initial-element '0))
-				       (list 'y 'z 'v)))
+				       (list :y :z :v)))
 
 (defparameter *non-export-symbol-list* '("*MAX-PARAM-SIZE*"
 					 "*ARG-SIZE*"
@@ -71,38 +71,38 @@
     (is-type empty-procedure-pool 'com.libgirl.smcl::procedure-pool)
     (is-type cl-user::procedures 'hash-table)
     (is (hash-table-count cl-user::procedures) 0)
-    (is (com.libgirl.smcl::reduce-f 'x
+    (is (com.libgirl.smcl::reduce-f :x
 				    (com.libgirl.smcl::make-procedure)
 				    empty-procedure-pool)
-	'x))
+	:x))
   (finalize))
 
 
 (defun test-simple-case (subtest-name subtest-number cl-user::procedure-pool)
   (subtest subtest-name
     (plan subtest-number)
-    (is (com.libgirl.smcl::get-procedure 'v cl-user::procedure-pool)
+    (is (com.libgirl.smcl::get-procedure :v cl-user::procedure-pool)
 	nil)
-    (is (com.libgirl.smcl::reduce-f (com.libgirl.smcl::procedure-body (com.libgirl.smcl::get-procedure 'x
+    (is (com.libgirl.smcl::reduce-f (com.libgirl.smcl::procedure-body (com.libgirl.smcl::get-procedure :x
 												       cl-user::procedure-pool))
-				    (com.libgirl.smcl::get-procedure 'x cl-user::procedure-pool)
+				    (com.libgirl.smcl::get-procedure :x cl-user::procedure-pool)
 				    cl-user::procedure-pool)
-	'v)
-    (is (com.libgirl.smcl::procedure-body (com.libgirl.smcl::get-procedure 'x
+	:v)
+    (is (com.libgirl.smcl::procedure-body (com.libgirl.smcl::get-procedure :x
 									   cl-user::procedure-pool))
-	'y)
-    (is (com.libgirl.smcl::procedure-body (com.libgirl.smcl::get-procedure 'y
+	:y)
+    (is (com.libgirl.smcl::procedure-body (com.libgirl.smcl::get-procedure :y
 									   cl-user::procedure-pool))
-	'v)
-    (com.libgirl.smcl::reduce-f (com.libgirl.smcl::procedure-body (com.libgirl.smcl::get-procedure 'x
+	:v)
+    (com.libgirl.smcl::reduce-f (com.libgirl.smcl::procedure-body (com.libgirl.smcl::get-procedure :x
 												   cl-user::procedure-pool))
-				(com.libgirl.smcl::get-procedure 'x cl-user::procedure-pool)
+				(com.libgirl.smcl::get-procedure :x cl-user::procedure-pool)
 				cl-user::procedure-pool
 				:set-procedure-new-body-p t)
-    (is (com.libgirl.smcl::procedure-body (com.libgirl.smcl::get-procedure 'x
+    (is (com.libgirl.smcl::procedure-body (com.libgirl.smcl::get-procedure :x
 									   cl-user::procedure-pool))
-	'v)
-    (is (com.libgirl.smcl::get-procedure 'v cl-user::procedure-pool)
+	:v)
+    (is (com.libgirl.smcl::get-procedure :v cl-user::procedure-pool)
 	nil)
     (finalize)))
 
@@ -135,12 +135,12 @@
 
 (subtest "test normal case but without parameter"
   (plan (fifth *subtest-number-list*))
-  (let ((procedure-name-list (list 'x 'y 'v 'z 'a))
-	(procedure-body-list (list (list 'y 'z)
-				   (list 'v 'u)
-				   'w
-				   (list 'a 'b)
-				   'c)))
+  (let ((procedure-name-list (list :x :y :v :z :a))
+	(procedure-body-list (list (list :y :z)
+				   (list :v :u)
+				   :w
+				   (list :a :b)
+				   :c)))
     (let* ((cl-user::procedure-pool (make-instance 'com.libgirl.smcl::procedure-pool
 						   :init-procedures (mapcar #'list
 									    procedure-name-list
@@ -149,7 +149,7 @@
 										       :initial-element (make-list com.libgirl.smcl::*arg-size*
 														   :initial-element '0))
 									    procedure-body-list)))
-	   (procedure-x (com.libgirl.smcl::get-procedure 'x
+	   (procedure-x (com.libgirl.smcl::get-procedure :x
 							 cl-user::procedure-pool)))
       ;;after initialization, name-body corresponding should be still the same
       (test-multiple-name-body-pairs procedure-name-list
@@ -160,45 +160,45 @@
 				  cl-user::procedure-pool
 				  :set-procedure-new-body-p t)
       (test-multiple-name-body-pairs (append procedure-name-list
-					     (list 'u 'b))
-				     (list 'w 'w 'w 'c 'c nil nil)
+					     (list :u :b))
+				     (list :w :w :w :c :c nil nil)
 				     cl-user::procedure-pool)))
   (finalize))
 
 (subtest "test normal case with parameters"
   (plan (sixth *subtest-number-list*))
-  (let* ((procedure-name-list (list 'x 'y 'z 'b 'a '0 'c))
+  (let* ((procedure-name-list (list :x :y :z :b :a :0 :c))
 	 (procedure-param-list (list nil
-				     (list 'p1 'p2)
-				     (list 'p1 'p2)
-				     (list 'p1 'p2)
-				     (list 'p1)
-				     (list 'p1 'p2)
+				     (list :p1 :p2)
+				     (list :p1 :p2)
+				     (list :p1 :p2)
+				     (list :p1)
+				     (list :p1 :p2)
 				     nil))
 	 (procedure-arg-list (mapcar (lambda (raw-list)
 				       (append raw-list
 					       (make-list (- com.libgirl.smcl::*arg-size*
 							     (length raw-list))
 							  :initial-element '0)))
-				     (list (list 'x1 'x2)
+				     (list (list :x1 :x2)
 					   nil
-					   (list 'z1 'z2)
-					   (list 'b1 'b2)
-					   (list 'aa)
+					   (list :z1 :z2)
+					   (list :b1 :b2)
+					   (list :aa)
 					   nil
 					   nil)))
-	 (procedure-body-list (list (list 'y
-					  (list 'z 'y 'v)
-					  (list 'a
-						(list 'b 'c 'k)))
-				    (list 'p1 'p2 'k)
-				    (list 'p1
-					  (list 'b 'p2)
-					  'c)
-				    'p2
-				    'p1
-				    'p2
-				    'd))
+	 (procedure-body-list (list (list :y
+					  (list :z :y :v)
+					  (list :a
+						(list :b :c :k)))
+				    (list :p1 :p2 :k)
+				    (list :p1
+					  (list :b :p2)
+					  :c)
+				    :p2
+				    :p1
+				    :p2
+				    :d))
 	 (cl-user::procedure-pool (make-instance 'com.libgirl.smcl::procedure-pool
 						 :init-procedures (mapcar #'list
 									  procedure-name-list
